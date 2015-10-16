82aa1ec5130e5142818920a537b3135a7e13b2e5
Joseph Frangoudes <jfrangoudes@alertlogic.com>
2015-10-16 14:04:45 +0100

    Fixed broken list_stacks_all function

--- src/erlcloud_cloudformation.erl
+++ src/erlcloud_cloudformation.erl
@@ -10,19 +10,20 @@
 
 %% Cloud Formation API Functions
 -export ([list_stacks_all/2,
-		  list_stacks/3,
-		  list_stack_resources/2,
-		  describe_stack_resources/2,
-		  describe_stack_resource/3,
-		  describe_stacks_all/2,
-		  describe_stacks/3,
-		  get_stack_policy/2,
-		  get_template/2,
-		  get_template_summary/2,
-		  describe_account_limits/2,
-		  describe_account_limits_all/1,
-		  describe_stack_events_all/2,
-		  describe_stack_events/3]).
+          list_stacks/2,
+          list_stack_resources_all/2,
+          list_stack_resources/3,
+          describe_stack_resources/2,
+          describe_stack_resource/3,
+          describe_stacks_all/2,
+          describe_stacks/3,
+          get_stack_policy/2,
+          get_template/2,
+          get_template_summary/2,
+          describe_account_limits/2,
+          describe_account_limits_all/1,
+          describe_stack_events_all/2,
+          describe_stack_events/3]).
 
 %%==============================================================================
 %% Cloud Formation API Functions
@@ -30,116 +31,121 @@
 
 -spec list_stacks_all(params(), aws_config()) -> {ok, cloudformation_list()}.
 list_stacks_all(Params, Config = #aws_config{}) ->
+    list_all(fun list_stacks/2, Params, Config, []).
 
-	ExtraParams = lists:map(fun(T) ->
-		case T of
-			{stack_status_filter, N, Filter} ->
-				{io_lib:format("StackStatusFilter.member.~p", [N]), Filter}
-			end
-		end, Params),
-	{ok, XmlNode} = cloudformation_request(Config, "ListStacks", ExtraParams),
-	extract_stack_summaries(xmerl_xpath:string("/ListStacksResponse/ListStacksResult", XmlNode)).
+-spec list_stacks(params(), aws_config()) -> {ok, cloudformation_list()}.
+list_stacks(Params, Config = #aws_config{}) ->
 
--spec list_stacks(params(), string(), aws_config()) -> {ok, cloudformation_list()}.
-list_stacks(Params, NextNode, Config = #aws_config{}) ->
+    ExtraParams = lists:map(fun(T) ->
+            case T of
+                {stack_status_filter, N, Filter} ->
+                    {io_lib:format("StackStatusFilter.member.~p", [N]), Filter};
 
-	ExtraParams = lists:map(fun(T) ->
-		case T of
-			{stack_status_filter, N, Filter} ->
-				{io_lib:format("StackStatusFilter.member.~p", [N]), Filter}
-			end
-		end, Params),
+                _ -> T
+            end
+        end, Params),
 
-	cloudformation_request(Config, "ListStacks", [{"NextNode", NextNode} | ExtraParams]).
+    {ok, XmlNode} = cloudformation_request(Config, "ListStacks", ExtraParams),
 
--spec list_stack_resources(string(), aws_config()) -> {ok, cloudformation_list()}.
-list_stack_resources(StackName, Config = #aws_config{}) ->
-	cloudformation_request(Config, "ListStackResources", [{"StackName", StackName}]).
+    NextToken = erlcloud_xml:get_text("/ListStacksResponse/ListStacksResult/NextToken", XmlNode, undefined),
+
+    [{next_token, NextToken}, extract_stack_summaries(xmerl_xpath:string("/ListStacksResponse/ListStacksResult", XmlNode))].
+
+-spec list_stack_resources_all(string(), aws_config()) -> {ok, cloudformation_list()}.
+list_stack_resources_all(Params, Config = #aws_config{}) ->
+    % cloudformation_request(Config, "ListStackResources", [{"StackName", StackName}]).
+    list_all(fun list_stack_resources/3, Params, Config, []).
+
+-spec list_stack_resources(string(), string(), aws_config()) -> {ok, cloudformation_list()}.
+list_stack_resources(NextToken, Params, Config = #aws_config{}) ->
+    cloudformation_request(Config, "ListStackResources", [{"NextToken", NextToken} 
+                                                          | Params
+                                                         ]).
 
 -spec describe_stack_resources(string(), aws_config()) -> {ok, cloudformation_list()}.
 describe_stack_resources(StackName, Config = #aws_config{}) ->
-	cloudformation_request(Config, "DescribeStackResources", [{"StackName", StackName}]).
+    cloudformation_request(Config, "DescribeStackResources", [{"StackName", StackName}]).
 
 -spec describe_stack_resource(string(), string(), aws_config) -> {ok, cloudformation_list()}.
 describe_stack_resource(StackName, LogicalResourceId, Config = #aws_config{}) ->
-	cloudformation_request(Config, "DescribeStackResource", [{"StackName", StackName},
-															 {"LogicalResourceId", LogicalResourceId}
-															]).
+    cloudformation_request(Config, "DescribeStackResource", [{"StackName", StackName},
+                                                             {"LogicalResourceId", LogicalResourceId}
+                                                            ]).
 
 -spec describe_stacks_all(params(), aws_config()) -> {ok, cloudformation_list()}.
 describe_stacks_all(Params, Config = #aws_config{}) ->
 
-	ExtraParams = lists:map(fun(T) ->
-		case T of
-			{stack_name, StackName} -> {"StackName", StackName}
-		end
-	end, Params),
+    ExtraParams = lists:map(fun(T) ->
+        case T of
+            {stack_name, StackName} -> {"StackName", StackName}
+        end
+    end, Params),
 
-	cloudformation_request(Config, "DescribeStacks", ExtraParams).
+    cloudformation_request(Config, "DescribeStacks", ExtraParams).
 
 -spec describe_stacks(params(), string(), aws_config()) -> {ok, cloudformation_list()}.
 describe_stacks(Params, NextNode, Config = #aws_config{}) ->
 
-	ExtraParams = lists:map(fun(T) ->
-		case T of
-			{stack_name, StackName} -> {"StackName", StackName}
-		end
-	end, Params),
+    ExtraParams = lists:map(fun(T) ->
+        case T of
+            {stack_name, StackName} -> {"StackName", StackName}
+        end
+    end, Params),
 
-	cloudformation_request(Config, "DescribeStacks", [{"NextNode", NextNode}, ExtraParams]).
+    cloudformation_request(Config, "DescribeStacks", [{"NextNode", NextNode}, ExtraParams]).
 
 -spec get_stack_policy(string(), aws_config()) -> {ok, cloudformation_list()}.
 get_stack_policy(StackName, Config = #aws_config{}) ->
-	cloudformation_request(Config, "GetStackPolicy", [{"StackName", StackName}]).
+    cloudformation_request(Config, "GetStackPolicy", [{"StackName", StackName}]).
 
 -spec describe_stack_events_all(params(), aws_config()) -> {ok, cloudformation_list()}.
 describe_stack_events_all(Params, Config = #aws_config{}) ->
 
-	ExtraParams = lists:map(fun(T) ->
-		case T of
-			{stack_name, StackName} -> {"StackName", StackName}
-		end
-	end, Params),
+    ExtraParams = lists:map(fun(T) ->
+        case T of
+            {stack_name, StackName} -> {"StackName", StackName}
+        end
+    end, Params),
 
-	cloudformation_request(Config, "DescribeStackEvents", ExtraParams).
+    cloudformation_request(Config, "DescribeStackEvents", ExtraParams).
 
 -spec describe_stack_events(params(), string(), aws_config()) -> {ok, cloudformation_list()}.
 describe_stack_events(Params, NextToken, Config = #aws_config{}) ->
 
-	ExtraParams = lists:map(fun(T) ->
-		case T of
-			{stack_name, StackName} -> {"StackName", StackName}
-		end
-	end, Params),
+    ExtraParams = lists:map(fun(T) ->
+        case T of
+            {stack_name, StackName} -> {"StackName", StackName}
+        end
+    end, Params),
 
-	cloudformation_request(Config, "DescribeStackEvents", [{"NextToken", NextToken} | ExtraParams]).
+    cloudformation_request(Config, "DescribeStackEvents", [{"NextToken", NextToken} | ExtraParams]).
 
 -spec get_template(string(), aws_config()) -> {ok, cloudformation_list()}.
 get_template(StackName, Config = #aws_config{}) ->
 
-	cloudformation_request(Config, "GetTemplate", [{"StackName", StackName}]).
+    cloudformation_request(Config, "GetTemplate", [{"StackName", StackName}]).
 
 -spec get_template_summary(params(), aws_config()) -> {ok, cloudformation_list()}.
 get_template_summary(Params, Config = #aws_config{}) ->
 
-	ExtraParams = lists:map(fun(T) ->
-		case T of
-			{template_url, URL} -> {"TemplateURL", URL};
-			{template_body, Body} -> {"TemplateBody", Body};
-			{stack_name, StackName} -> {"StackName", StackName}
-		end
-	end, Params),
+    ExtraParams = lists:map(fun(T) ->
+        case T of
+            {template_url, URL} -> {"TemplateURL", URL};
+            {template_body, Body} -> {"TemplateBody", Body};
+            {stack_name, StackName} -> {"StackName", StackName}
+        end
+    end, Params),
 
-	cloudformation_request(Config, "GetTemplateSummary", ExtraParams).
+    cloudformation_request(Config, "GetTemplateSummary", ExtraParams).
 
 -spec describe_account_limits_all(aws_config()) -> {ok, cloudformation_list()}.
 describe_account_limits_all(Config = #aws_config{}) ->
 
-	cloudformation_request(Config, "DescribeAccountLimits", []).
+    cloudformation_request(Config, "DescribeAccountLimits", []).
 
 -spec describe_account_limits(aws_config(), string()) -> {ok, cloudformation_list()}.
 describe_account_limits(Config = #aws_config{}, NextToken) ->
-	cloudformation_request(Config, "DescribeAccountLimits", [{"NextToken", NextToken}]).
+    cloudformation_request(Config, "DescribeAccountLimits", [{"NextToken", NextToken}]).
 
 %%==============================================================================
 %% Internal functions
@@ -147,28 +153,36 @@ describe_account_limits(Config = #aws_config{}, NextToken) ->
 
 cloudformation_request(Config = #aws_config{}, Action, ExtraParams) ->
 
-	QParams = [
-		{"Action", Action},
-		{"Version", ?API_VERSION}
-		| ExtraParams],
+    QParams = [
+        {"Action", Action},
+        {"Version", ?API_VERSION}
+        | ExtraParams],
 
-	erlcloud_aws:aws_request_xml4(post, Config#aws_config.cloudformation_host, "/", QParams, "cloudformation", Config).
+    erlcloud_aws:aws_request_xml4(post, Config#aws_config.cloudformation_host, "/", QParams, "cloudformation", Config).
 
-extract_stack_summaries(XmlNode) ->
-	lists:map(fun(T) -> erlcloud_xml:decode([{summaries, "StackSummaries", {optional_map, fun extract_stacks/1}}], T) end, XmlNode).
+list_all(Fun, Params, Config, Acc) ->
+    List = Fun(Params, Config),
+    case proplists:get_value(next_token, List, not_here) of
+        undefined ->
+            lists:foldl(fun erlang:'++'/2, [], [List | Acc]);
+        NextToken ->
+            list_all(Fun, [{"NextToken", NextToken} | Params], Config, [List | Acc])
+    end.
+
+extract_stack_summaries(XmlNodes) ->
+    lists:map(fun(T) -> erlcloud_xml:decode([{summaries, "StackSummaries", {optional_map, fun extract_stacks/1}}], T) end, XmlNodes).
 
 extract_stacks(XmlNode) ->
-	erlcloud_xml:decode([
-			{member, "member", {optional_map, fun extract_stack/1}}
-		], XmlNode).
+    erlcloud_xml:decode([
+            {member, "member", {optional_map, fun extract_stack/1}}
+        ], XmlNode).
 
 extract_stack(XmlNode) ->
-	erlcloud_xml:decode([
-		{stack_id, "StackId", optional_text},
-		{stack_status, "StackStatus", optional_text},
-		{stack_name, "StackName", optional_text},
-		{creation_time, "CreationTime", optional_text},
-		{template_description, "TemplateDescription", optional_text},
-		{resource_types, "ResourceTypes", list}], XmlNode).
-
+    erlcloud_xml:decode([
+        {stack_id, "StackId", optional_text},
+        {stack_status, "StackStatus", optional_text},
+        {stack_name, "StackName", optional_text},
+        {creation_time, "CreationTime", optional_text},
+        {template_description, "TemplateDescription", optional_text},
+        {resource_types, "ResourceTypes", list}], XmlNode).
 
